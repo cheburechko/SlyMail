73286758597e18a6e6d87b1012cf3702794c0d14
Pavel <cheburechko@localhost.localdomain>
2013-12-08 16:04:26 +0659

    Added models for messages adn users.

--- SlyMail/settings.py
+++ SlyMail/settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Cherenkov Pavel', 'cherenkov.p.a@yandex.ru'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'storage.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -115,10 +115,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    #'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
--- client/models.py
+++ client/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=30, primary_key=True);
+    password = models.CharField(max_length=128)
+
+
+class AddressBook(models.Model):
+    owner = models.ForeignKey(to=User)
+    email = models.EmailField()
+
+
+class Message(models.Model):
+    sender = models.EmailField()
+    owner = models.ForeignKey(to=User)
+    date = models.DateTimeField()
+    subject = models.TextField()
+    raw_message = models.FileField()
+
+
+class MessagePart(models.Model):
+    message = models.ForeignKey(to=Message)
+    content_type = models.CharField(max_length=100)
+    file_path = models.FileField()
