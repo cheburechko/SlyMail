3e77d85babc550855c1158d91b6da0da650957c8
Pavel <cheburechko@localhost.localdomain>
2013-12-25 21:20:14 +0659

    Added address book editing/delteing/adding.

--- client/urls.py
+++ client/urls.py
@@ -16,10 +16,16 @@ urlpatterns = patterns('',
     url(r'^sent$', 'client.views.list.sent', name='sent'),
     url(r'^trash$', 'client.views.list.trash', name='trash'),
     url(r'^drafts$', 'client.views.list.drafts', name='drafts'),
-    url(r'^edit/(?P<pk>\d+)$', 'client.views.processSingle.edit', name='edit'),
+
     url(r'^read/(?P<pk>\d+)$', 'client.views.fetch.read', name='read'),
     url(r'^read/(?P<pk>\d+)/fetch$', 'client.views.fetch.fetchMail', name='fetchMail'),
     url(r'^download/(?P<pk>\d+)$', 'client.views.fetch.download', name='download'),
+
     url(r'^process$', 'client.views.process.process', name="process"),
-    url(r'^process/(?P<pk>\d+)$', 'client.views.processSingle.processSingle', name="processSingle")
+    url(r'^process/(?P<pk>\d+)$', 'client.views.processSingle.processSingle', name="processSingle"),
+    url(r'^edit/(?P<pk>\d+)$', 'client.views.processSingle.edit', name='edit'),
+
+    url(r'^settings$', 'client.views.user.settings', name='settings'),
+    url(r'^address_book$', 'client.views.user.address_book', name='address_book'),
+    url(r'^files$', 'client.views.user.files', name='files'),
 )
